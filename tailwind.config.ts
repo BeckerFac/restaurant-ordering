4d8c90433c2db427ce9be919c4aba8584aee1929
BeckerFac <facundobecker000@gmail.com>
2025-05-17 14:09:08 -0300

    Fix: configuración para Render, Tailwind y limpieza de archivos de Netlify

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,9 @@ const config: Config = {
     darkMode: ["class"],
     content: [
       "./app/**/*.{js,ts,jsx,tsx,mdx}",
-      "./components/**/*.{js,ts,jsx,tsx,mdx}"
+      "./components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./styles/**/*.{js,ts,jsx,tsx,mdx}"
     ],
   theme: {
   	extend: {
